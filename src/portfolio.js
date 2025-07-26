a3f3ce60a9f7003d9adea3f22ef6c765be6ed9a7
PJ Sathischandar <prajitchandar@gmail.com>
2025-07-26 17:33:54 -0400

    Fix Purdue logo path and add proper Purdue University logo

--- src/portfolio.js
+++ src/portfolio.js
@@ -124,7 +124,7 @@ const educationInfo = {
   schools: [
     {
       schoolName: "Purdue University West Lafayette",
-      logo: require("/Users/pjsathischandar/Documents/my projects/my portfolio/developerFolio/src/assets/images/purdue-university.svg"),
+      logo: require("./assets/images/purdueLogo.png"),
       subHeader: "Bachelors of Science in Computer Science and AI",
       duration: "Aug 2023 - Present",
       desc: "Participated in the research of human-assistance robotics.",
